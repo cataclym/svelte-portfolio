240fff3cbe16d6dfa84b91432e2af5314d141cf7
Cata <11278743+cataclym@users.noreply.github.com>
2024-01-02 23:59:28 +0100

    Fix for apostrophe in filename

--- optimize.js
+++ optimize.js
@@ -11,6 +11,26 @@ try {
 /** @constant
  * 	@type {string[]}
  */
+const toBeDeleted = await new Promise((resolve, reject) => fs.readdir("./src/assets/illustrations", undefined, (err, files) => err
+	? reject(err)
+	: resolve(files)
+))
+
+toBeDeleted.shift()
+
+console.log("Deleting contents of src/assets/illustrations")
+
+Promise.all(toBeDeleted.map(file => new Promise((resolve, reject) => fs.unlink(`./src/assets/illustrations/${file}`, (err) => err
+	? reject(err)
+	: resolve())
+)))
+	.then(console.log("Content deleted.\n"))
+
+console.log("Converting PNGs to WEBP:")
+
+/** @constant
+ * 	@type {string[]}
+ */
 const files = await new Promise((resolve, reject) =>
 	fs.readdir('./static/illustrations/', undefined, (err, files) => {
 		if (err) reject(err);
@@ -23,7 +43,7 @@ files.shift()
 const mapped = await Promise.all(files.map(async (f) => JSON.stringify(f)));
 
 for (const file of mapped) {
-	const query = `./static/illustrations/${file} -o ./src/assets/illustrations/${file.replace('.png', '.webp')}`;
+	const query = `./static/illustrations/${file} -o ./src/assets/illustrations/${file.replaceAll("'", "").replace('.png', '.webp')}`;
 	void new Promise((resolve, reject) =>
 		child_process.exec(
 			`cwebp ${query}`,
